07be638fd22bf0b974f4bb82dbfb1bdf4093b0d7
Isabelly Barbosa <isabelly.leite1998@gmail.com>
2025-01-21 18:17:53 -0300

    Somando numeros reais e inteiros com python

---
+++ juntandonumeros.py
@@ -0,0 +1,4 @@
+n1 = input('Digite um valor:')
+n2 = input('Digite outro valor:')
+s = n1 + n2
+print("A junção é", s)
---
+++ somando.py
@@ -0,0 +1,13 @@
+n1 = int(input('Digite um valor:'))
+n2 = int(input('Digite outro valor:'))
+s = n1 + n2
+print('A soma vale', s)
+# Outra forma
+print('A soma entre: {} e {} , vale {}.'.format(n1,n2,s))
+# Somando números reais
+n1 = float(input('Digite um valor:'))
+n2 = float(input('Digite outro valor:'))
+s = n1 + n2 
+print('A soma vale',s)
+# Outra forma
+print('A soma entre: {} e {} vale {}.'.format(n1,n2,s))
