0caa480a7fa3341e8de83f08bc0b776e2fe868fc
Isabelly Barbosa <isabelly.leite1998@gmail.com>
2025-01-23 13:19:44 -0300

    Aprendendo a calcular antecessor e sucessor

---
+++ antecessoresucessor.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
+n = int(input('Digite um número:'))
+a = n - 1
+s = n + 1
+print('O sucessor deste número é {} e o antecessor é {}'.format(s,a))
+#Outra forma
+n = int(input('Digite um número:'))
+print('O sucessor deste número é {} e o antecessor é {}'.format((n+1),(n-1)))
+      
