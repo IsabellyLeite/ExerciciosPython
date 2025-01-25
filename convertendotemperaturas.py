e0cd3c84fe4ca3e3dabcc38a3a524fbac8ea2dd7
Isabelly Barbosa <isabelly.leite1998@gmail.com>
2025-01-25 14:06:12 -0300

    Convertendo temperaturas de °C PARA °F.

---
+++ convertendotemperaturas.py
@@ -0,0 +1,4 @@
+#Escreva um programa que converta uma temperatura digitada em °C e converta em °F.
+c = float(input('Informe uma temperatura em °C:'))
+f = ((9*c)/5)+32
+print('A temperatura de {}°C, correnponde a {}°F.'.format(c, f))
